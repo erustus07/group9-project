5c2dd8f77d75ce429d0c9a3622b58ca360476c67
erustus07 <erustusndegwa08@gmail.com>
2024-07-29 14:35:36 +0300

    searchHistory model

--- server/models.py
+++ server/models.py
@@ -21,3 +21,25 @@ class User(db.Model):
     def create_user(self):
         db.session.add(self)
         db.session.commit()
+
+
+ # SearchHistory Model
+class SearchHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    query = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def add_search_history(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_search_history(user_id):
+        return SearchHistory.query.filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def clear_search_history(user_id):
+        SearchHistory.query.filter_by(user_id=user_id).delete()
+        db.session.commit()
+ 
