3178bbc953d6db1d3aef7956d49788b4fdc8e7c1
erustus07 <erustusndegwa08@gmail.com>
2024-07-29 14:40:57 +0300

    query model

--- server/models.py
+++ server/models.py
@@ -76,3 +76,21 @@ class Product(db.Model):
     def get_product_by_id(product_id):
         return Product.query.get(product_id)
 
+
+# Query Model
+class Query(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    search_term = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def create_query(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_query_by_id(query_id):
+        return Query.query.get(query_id)
+
+    @staticmethod
+    def get_all_queries():
+        return Query.query.all()
