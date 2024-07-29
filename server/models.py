46071c105e41ebc8ad749c795773c15191420f06
erustus07 <erustusndegwa08@gmail.com>
2024-07-29 14:37:31 +0300

    product model

--- server/models.py
+++ server/models.py
@@ -43,3 +43,36 @@ class SearchHistory(db.Model):
         SearchHistory.query.filter_by(user_id=user_id).delete()
         db.session.commit()
  
+# Product Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    delivery_cost = db.Column(db.Float, nullable=False)
+    payment_mode = db.Column(db.String(50), nullable=False)
+    shop = db.Column(db.String(100), nullable=False)
+    query_id = db.Column(db.Integer, db.ForeignKey('query.id'), nullable=False)
+    query = db.relationship('Query', backref='products', lazy=True)
+
+    def add_product(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_products_by_query(query_id):
+        return Product.query.filter_by(query_id=query_id).all()
+
+    @staticmethod
+    def update_product(product_id, **kwargs):
+        product = Product.query.get(product_id)
+        if product:
+            for key, value in kwargs.items():
+                setattr(product, key, value)
+            db.session.commit()
+        return product
+
+    @staticmethod
+    def get_product_by_id(product_id):
+        return Product.query.get(product_id)
+
