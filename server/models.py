2066471b2a88392815274f951eb2981b723f4547
erustus07 <erustusndegwa08@gmail.com>
2024-07-29 14:43:48 +0300

    marginal benefits

--- server/models.py
+++ server/models.py
@@ -94,3 +94,31 @@ class Query(db.Model):
     @staticmethod
     def get_all_queries():
         return Query.query.all()
+
+
+# MarginalBenefitCalculation Model
+class MarginalBenefitCalculation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    mb_score = db.Column(db.Float, nullable=False)
+    cb_score = db.Column(db.Float, nullable=False)
+    product = db.relationship('Product', backref='mb_calculations', lazy=True)
+
+    @staticmethod
+    def calculate_mb(product_id, mb_score):
+        mb_calc = MarginalBenefitCalculation(product_id=product_id, mb_score=mb_score, cb_score=0.0)
+        db.session.add(mb_calc)
+        db.session.commit()
+        return mb_calc
+
+    @staticmethod
+    def calculate_cb(product_id, cb_score):
+        mb_calc = MarginalBenefitCalculation.query.filter_by(product_id=product_id).first()
+        if mb_calc:
+            mb_calc.cb_score = cb_score
+            db.session.commit()
+        return mb_calc
+
+    @staticmethod
+    def get_mb_cb_by_product(product_id):
+        return MarginalBenefitCalculation.query.filter_by(product_id=product_id).first()
