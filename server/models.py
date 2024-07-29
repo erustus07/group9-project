8eaca4b34ed5f89ba0b6b14f0ca42d3e17329483
erustus07 <erustusndegwa08@gmail.com>
2024-07-29 14:26:25 +0300

    usermodel

---
+++ server/models.py
@@ -0,0 +1,23 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    search_history = db.relationship('SearchHistory', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def create_user(self):
+        db.session.add(self)
+        db.session.commit()
